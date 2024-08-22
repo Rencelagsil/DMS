e447c018e129dce8f77e967fe22ad7c103629f19
Rencelagsil <142850140+Rencelagsil@users.noreply.github.com>
2024-08-22 20:57:52 +0800

    Update for now main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,28 @@ void changeDirectory();
 
 int main()
 {
+  int choice;
+  do {
+    displayMenu();
+    std::cin >>choice;
+    switch(choice) {
+      case 1:
+      displayFiles();
+      break;
+      case 2:
+      createDirectory();
+      break;
+      case 3:
+      changeDirectory();
+      break;
+      case 4:
+        std::cout << "Exiting the program." << std::endl;
+      break;
+    }
+    std::cin.ignore();
+    std::cin.get();
+    
+  } while (choice!= 4);
   
     return 0;
 }
